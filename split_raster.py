9f159f12396fc97c3bacd88ecbaefa9e0a17402b
Bang Pham Huu <b.phamhuu@jacobs-university.de>
2018-10-09 12:10:09 +0200

    First commit

---
+++ split_raster.py
@@ -0,0 +1,46 @@
+import os
+import sys
+import gdal
+
+# Author: Bang Pham Huu - https://github.com/bangph
+# Split big raster which rasdaman cannot import to tile with size 20000 x 20000
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+if len(sys.argv) == 1:
+    print "Usage: python script.py ABSOLUTE_PATH_TO_BIG_TIF_FILE"
+    exit(0)
+
+# Split tile to this size 
+tile_size_x = 20000
+tile_size_y = 20000
+
+big_file_path = sys.argv[1]
+
+if not os.path.isabs(big_file_path):
+    print "Path to input big tiff file must be absolute, given '{}'.".format(big_file_path)
+    exit(1)
+
+# Folder containing big tiff file
+input_directory_path = os.path.dirname(big_file_path)
+
+# File name (without extension)
+big_file_name = os.path.splitext(os.path.basename(big_file_path))[0]
+ 
+# Create output folder to store tiles of this big tiff file
+out_path = input_directory_path + '/split_output/' + big_file_name + "/"
+if not os.path.exists(out_path):
+    os.makedirs(out_path)
+
+output_filename = 'tile_'
+ 
+ds = gdal.Open(big_file_path)
+band = ds.GetRasterBand(1)
+xsize = band.XSize
+ysize = band.YSize
+ 
+for i in range(0, xsize, tile_size_x):
+    for j in range(0, ysize, tile_size_y):
+        tile_file_path = str(out_path) + str(output_filename) + str(i) + "_" + str(j) + ".tif"
+        com_string = "gdal_translate -of GTIFF " + ' -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "TILED=YES" -srcwin ' + str(i)+ ", " + str(j) + ", " + str(tile_size_x) + ", " + str(tile_size_y) + " " + big_file_path + " " + tile_file_path
+        os.system(com_string)
+
